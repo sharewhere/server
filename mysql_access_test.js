babe0061b4abdf4a2adeb4e7375faa575f8d3f1b
Jimmy Campbell <jimmypcampbell92@yahoo.com>
2015-02-14 19:30:04 +0000

    added mysql_access_test.js
    This is a testing script for ensuring access to mysql is working.

--- mysql_access_test.js
+++ mysql_access_test.js
@@ -2,5 +2,23 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'ShareWhereUser',
-  password : 'N3onIced'
+  password : 'N3onIc3d',
+  database : 'sharewheretest'
 });
+
+connection.connect();
+
+connection.query('SELECT * from ranks', function(err, rows, fields) {
+  if (err) throw err;
+
+ var numRows = rows.length;
+
+ for(var i=0;i<numRows;i++){
+	 console.log(rows[i]['rank_title']);
+ } 
+ 
+
+  
+});
+
+connection.end();
