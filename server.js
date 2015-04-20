sharewhere/server

commit 57a460a117cb9faea8515a66ce13b2cbe9477e2c

Fixed issue while dereferencing a null array

server.js
@@ -291,8 +291,11 @@ app.get('/viewreqoffshareable', restrict, function(req, res) {
             })
             return;
         }
-        if(trnsction.length<1) trnsction = null;
-        if(shrble.username == req.session.user.username) {
+        if(trnsction.length == 0) {
+            res.json({success: true, shareable : shrble, transactions : null});
+            return;
+        }
+        else if(shrble.username == req.session.user.username) {
             res.json({success: true, shareable : shrble, transactions : trnsction});
             return;
         }

commit e265bb0b2ac25d357837c2ae403527f3d2b84f4d

viewreqoffshareable change
Has to return object with property "transactions" if you are the creator
of the shareable.
Has to return object with property "transaction" if you are not the
creater.

This was broken in commit 57a460a117cb9faea8515a66ce13b2cbe9477e2c
because it was always returning transactions if trnsction array was
empty.

server.js
@@ -291,17 +291,25 @@ app.get('/viewreqoffshareable', restrict, function(req, res) {
             })
             return;
         }
-        if(trnsction.length == 0) {
-            res.json({success: true, shareable : shrble, transactions : null});
-            return;
-        }
-        else if(shrble.username == req.session.user.username) {
-            res.json({success: true, shareable : shrble, transactions : trnsction});
-            return;
+        if(shrble.username == req.session.user.username) {
+            if(trnsction.length <1) {
+                res.json({success: true, shareable : shrble, transactions : null});
+                return;
+            }
+            else{
+                res.json({success: true, shareable : shrble, transactions : trnsction});
+                return;
+            }
         }
         else {
-            res.json({success: true, shareable : shrble, transaction : trnsction[0]});
-            return;
+            if(trnsction.length <1) {
+                res.json({success: true, shareable : shrble, transaction : null});
+                return;
+            }
+            else{
+                res.json({success: true, shareable : shrble, transaction : trnsction[0]});
+                return;
+            }
         }
     });
 });